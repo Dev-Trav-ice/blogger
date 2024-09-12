8e9a714edf9f5e5d82fb42039c5ddc78dfc35c39
Muganda <butunyidennis@gmail.com>
2024-09-12 18:35:25 +0300

    configured and setup the backend and the folder structure. also connected the db.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { db } from "./db.js";
 
 dotenv.config();
 
@@ -9,5 +12,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+  console.log(`listening...`);
+});
