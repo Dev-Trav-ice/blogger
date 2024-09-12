3383591630cfe4dcbe73ba229bbbec3f7e82e086
Muganda <butunyidennis@gmail.com>
2024-09-12 18:05:58 +0300

    sign-in and sign-up component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <div className="flexCenter flex-col h-[calc(100vh-70px)]">
+        <h1 className="bold-32 text-center font-serif">Sign In</h1>
+        <p className="text-center font-serif medium-18 m-4">
+          Please sign in to your account to continue
+        </p>
+        <form className="flex flex-col gap-5 w-[270px]">
+          <div className="p-3 bg-gray-300">
+            <input
+              type="email"
+              placeholder="email"
+              className="border-none bg-transparent outline-none w-full placeholder:text-black/50"
+            />
+          </div>
+
+          <div className="p-3 bg-gray-300">
+            <input
+              type="password"
+              placeholder="password"
+              className="border-none bg-transparent outline-none w-full placeholder:text-black/50"
+            />
+          </div>
+
+          <button className="p-3 bg-black text-white text-[16px] rounded-md">
+            Sign In
+          </button>
+          <p className=" medium-18 font-serif">
+            Don't have an account?{" "}
+            <Link className="underline" to="/sign-up">
+              Sign up
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <div className="flexCenter flex-col h-[calc(100vh-70px)]">
+        <h1 className="bold-32 text-center font-serif">Sign Up</h1>
+        <p className="text-center font-serif medium-18 m-4">
+          Please create an account to continue
+        </p>
+        <form className="flex flex-col gap-5 w-[270px]">
+          <div className="p-3 bg-gray-300">
+            <input
+              type="text"
+              placeholder="username"
+              className="border-none bg-transparent outline-none w-full placeholder:text-black/50"
+            />
+          </div>
+
+          <div className="p-3 bg-gray-300">
+            <input
+              type="email"
+              placeholder="email"
+              className="border-none bg-transparent outline-none w-full placeholder:text-black/50"
+            />
+          </div>
+
+          <div className="p-3 bg-gray-300">
+            <input
+              type="password"
+              placeholder="password"
+              className="border-none bg-transparent outline-none w-full placeholder:text-black/50"
+            />
+          </div>
+
+          <button className="p-3 bg-black text-white text-[16px] rounded-md">
+            Sign Up
+          </button>
+          <p className=" medium-18 font-serif">
+            Already have an account?{" "}
+            <Link className="underline" to="/sign-in">
+              Sign in
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
