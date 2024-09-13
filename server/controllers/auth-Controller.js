1c7a6536243278c6426fb3c408d99e68aa7c5348
Muganda <butunyidennis@gmail.com>
2024-09-13 08:11:35 +0300

    sign up to db and tokenization

--- server/controllers/auth-Controller.js
+++ server/controllers/auth-Controller.js
@@ -1,29 +1,47 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const register = (req, res) => {
   const { username, email, password } = req.body;
 
-  const userExists = "SELECT * from user where email = ?";
-  db.query(userExists, [email], (err, results) => {
+  const q = "SELECT * FROM user WHERE username = ?";
+
+  db.query(q, [username], (err, results) => {
     if (err) throw err;
 
-    if (results.length > 0) {
-      return res.status(409).json({ error: "User already exists" });
-    }
+    if (results.length > 0)
+      return res.status(409).json({ message: "User already exists" });
 
     const hashedPassword = bcrypt.hashSync(password, 10);
 
-    const query =
-      "INSERT INTO user(`username`, `email`, `password` ) VALUES(?)";
+    const q = "INSERT INTO user (username, email, password) VALUES (? , ? , ?)";
 
-    db.query(query, [username, email, hashedPassword], (err, results) => {
+    db.query(q, [username, email, hashedPassword], (err, results) => {
       if (err) throw err;
 
-      return res
-        .status(200)
-        .json({ message: "User inserted successfully" }, results);
+      const q = "SELECT * FROM user WHERE id = ?";
+
+      db.query(q, [results.insertId], (err, data) => {
+        if (err) throw err;
+
+        const JWT_SECRET = process.env.jwt_secret;
+
+        const token = jwt.sign({ id: data[0].id }, JWT_SECRET, {
+          expiresIn: "5h",
+        });
+
+        const { password, ...others } = data[0];
+        res
+          .cookie("access_token", token, {
+            httpOnly: true,
+          })
+          .status(200)
+          .json(others);
+      });
     });
   });
 };
